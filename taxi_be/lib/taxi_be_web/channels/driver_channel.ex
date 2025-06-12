2a6a7205171c1a327ac7849a369cb25958c28329
iii <isaacmartinez@MacBook-Pro-de-Isaac-2.local>
2025-06-11 21:20:09 -0600

    driver_channel v2

--- taxi_be/lib/taxi_be_web/channels/driver_channel.ex
+++ taxi_be/lib/taxi_be_web/channels/driver_channel.ex
@@ -1,11 +1,8 @@
 defmodule TaxiBeWeb.DriverChannel do
   use TaxiBeWeb, :channel
 
-  alias TaxiBe.BookingProcess
-
   @impl true
   def join("driver:" <> _username, _payload, socket) do
-    # socket = assign(socket, :driver_id, driver_id_from_token)
     {:ok, socket}
   end
 
@@ -14,12 +11,20 @@ defmodule TaxiBeWeb.DriverChannel do
   def handle_in("accept_ride", %{"booking_id" => booking_id}, socket) do
     driver_data = %{id: 1, name: "Conductor de Prueba"}
 
-    case Registry.lookup(TaxiBe.BookingRegistry, booking_id) do
-      [{pid, _}] ->
-        GenServer.cast(pid, {:driver_accepted, driver_data})
-      _ ->
 
-        :ok
+    pid = find_booking_process(booking_id)
+
+    if pid do
+      # Usamos `call` para esperar una respuesta síncrona
+      case GenServer.call(pid, {:driver_accepted, driver_data}) do
+        :ok ->
+          # El conductor fue el primero, se le puede notificar en el front-end
+          push(socket, "ride_confirmed", %{status: "you_got_it"})
+        {:error, :already_taken} ->
+          # Alguien más tomó el viaje
+          push(socket, "ride_already_taken", %{status: "too_late"})
+        _ -> :ok
+      end
     end
 
     {:noreply, socket}
@@ -30,13 +35,21 @@ defmodule TaxiBeWeb.DriverChannel do
   def handle_in("reject_ride", %{"booking_id" => booking_id}, socket) do
     driver_data = %{id: 1, name: "Conductor de Prueba"}
 
-    case Registry.lookup(TaxiBe.BookingRegistry, booking_id) do
-      [{pid, _}] ->
-        GenServer.cast(pid, {:driver_rejected, driver_data})
-      _ ->
-        :ok
-    end
+    pid = find_booking_process(booking_id)
+    if pid, do: GenServer.call(pid, {:driver_rejected, driver_data})
 
     {:noreply, socket}
+  end
+
+  defp find_booking_process(booking_id) do
+    case Registry.lookup(TaxiBe.ConcurrentBookingRegistry, booking_id) do
+      [{pid, _}] -> pid
+      [] ->
+        # Si no está en el concurrente, buscamos en el secuencial
+        case Registry.lookup(TaxiBe.BookingRegistry, booking_id) do
+          [{pid, _}] -> pid
+          [] -> nil
+        end
+    end
   end
 end
