1a689a9ca172aa32cb09fe7b0d706e3a078e617e
iii <isaacmartinez@MacBook-Pro-de-Isaac-2.local>
2025-06-14 14:02:43 -0600

    genserver accepted state

--- taxi_be/lib/taxi_be/concurrent_booking_process.ex
+++ taxi_be/lib/taxi_be/concurrent_booking_process.ex
@@ -5,8 +5,9 @@ defmodule TaxiBe.ConcurrentBookingProcess do
   alias TaxiBe.Endpoint
   alias TaxiBe.Drivers
 
-  @timeout 90_000 # 90 segundos en milisegundos
+  @timeout 90_000
   @drivers_to_notify 3
+  @cancellation_fee_window 180
 
   def start_link(args) do
     GenServer.start_link(__MODULE__, args, name: via_tuple(args.booking.id))
@@ -16,7 +17,6 @@ defmodule TaxiBe.ConcurrentBookingProcess do
   def init(args) do
     %{booking: booking, customer_pid: customer_pid} = args
 
-
     drivers_to_notify =
       Drivers.get_available_drivers(booking.origin)
       |> Enum.take(@drivers_to_notify)
@@ -25,82 +25,90 @@ defmodule TaxiBe.ConcurrentBookingProcess do
       booking: booking,
       customer_pid: customer_pid,
       notified_drivers: drivers_to_notify,
-      accepted_by: nil, # Para guardar quién aceptó
+      accepted_by: nil,
+      eta: nil,
       timer: nil
     }
 
     if Enum.any?(drivers_to_notify) do
-
       for driver <- drivers_to_notify do
-        Logger.info("Notificando al conductor #{driver.id} para el booking ##{booking.id}")
         Endpoint.broadcast("driver:#{driver.username}", "new_ride_request", %{booking: booking})
       end
 
       timer = Process.send_after(self(), :timeout, @timeout)
-
       {:ok, %{state | timer: timer}}
     else
-      # No hay conductores, notificamos al cliente inmediatamente
-      Logger.info("No hay conductores disponibles para el booking ##{booking.id}")
       send(customer_pid, {:ride_not_fulfilled, %{reason: "No drivers available"}})
       {:stop, :normal, state}
     end
   end
 
-
-
   @impl true
   def handle_info(:timeout, state) do
-    Logger.info("Timeout de 90s alcanzado para el booking ##{state.booking.id}. Ningún conductor aceptó.")
     send(state.customer_pid, {:ride_not_fulfilled, %{reason: "No driver accepted the ride in time"}})
-    # Detenemos el proceso
     {:stop, :normal, %{state | timer: nil}}
   end
 
-
-  # --- Manejo de respuestas del conductor ---
-
-
   @impl true
   def handle_call({:driver_accepted, driver_data}, _from, state) do
-
     if is_nil(state.accepted_by) do
-      Logger.info("El conductor #{driver_data.id} fue el PRIMERO en aceptar el booking ##{state.booking.id}")
-
       if state.timer, do: Process.cancel_timer(state.timer)
 
-      # Notificar al cliente
-      send(state.customer_pid, {:ride_accepted, %{driver: driver_data, booking: state.booking}})
+      updated_booking = Map.put(state.booking, :status, "accepted")
 
-      {:reply, :ok, %{state | accepted_by: driver_data.id}}
+      send(state.customer_pid, {:ride_accepted, %{driver: driver_data, booking: updated_booking}})
+
+      new_state =
+        state
+        |> Map.put(:accepted_by, driver_data.id)
+        |> Map.put(:eta, driver_data.eta)
+        |> Map.put(:booking, updated_booking)
+
+      for driver <- state.notified_drivers do
+        if driver.id != driver_data.id do
+          Endpoint.broadcast("driver:#{driver.username}", "ride_cancelled", %{booking_id: state.booking.id})
+        end
+      end
+
+      {:reply, :ok, new_state}
     else
-      # La reserva ya fue tomada por otro conductor
-      Logger.info("El conductor #{driver_data.id} intentó aceptar, pero ya fue tomada por #{state.accepted_by}")
       {:reply, {:error, :already_taken}, state}
     end
   end
 
   @impl true
-  def handle_call({:driver_rejected, driver_data}, _from, state) do
-    Logger.info("El conductor #{driver_data.id} RECHAZÓ el booking ##{state.booking.id}")
+  def handle_call({:driver_rejected, _driver_data}, _from, state) do
     {:reply, :ok, state}
   end
 
-  # --- Finalización ---
   @impl true
-  def terminate(reason, state) do
+  def handle_call(:customer_cancelled, _from, state) do
+    if is_nil(state.accepted_by) do
+      {:reply, :ok_no_charge, state}
+    else
+      now = DateTime.utc_now()
+      diff_in_seconds = DateTime.diff(state.eta, now, :second)
 
-    if reason == :normal && state.accepted_by do
+      if diff_in_seconds > 0 and diff_in_seconds <= @cancellation_fee_window do
+        {:reply, :ok_charge_applied, state}
+      else
+        {:reply, :ok_no_charge, state}
+      end
+    end
+  end
+
+  @impl true
+  def terminate(_reason, state) do
+    if state.accepted_by do
+      accepted_driver_topic = "driver:#{state.accepted_by}"
+      Endpoint.broadcast(accepted_driver_topic, "ride_cancelled_by_customer", %{booking_id: state.booking.id})
+    else
       for driver <- state.notified_drivers do
-        # No se lo enviamos al que aceptó
-        if driver.id != state.accepted_by do
-          Endpoint.broadcast("driver:#{driver.username}", "ride_cancelled", %{booking_id: state.booking.id})
-        end
+        Endpoint.broadcast("driver:#{driver.username}", "ride_cancelled", %{booking_id: state.booking.id})
       end
     end
     :ok
   end
-
 
   defp via_tuple(booking_id) do
     {:via, Registry, {TaxiBe.ConcurrentBookingRegistry, booking_id}}
