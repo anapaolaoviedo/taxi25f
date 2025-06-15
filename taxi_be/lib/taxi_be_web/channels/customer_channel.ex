067d20cb389b10b68d49fa3011f3d3d16643f66b
iii <isaacmartinez@MacBook-Pro-de-Isaac-2.local>
2025-06-14 21:50:55 -0600

    custome_channel fix

--- taxi_be/lib/taxi_be_web/channels/customer_channel.ex
+++ taxi_be/lib/taxi_be_web/channels/customer_channel.ex
@@ -2,12 +2,13 @@ defmodule TaxiBeWeb.CustomerChannel do
   use TaxiBeWeb, :channel
 
   alias TaxiBe.ConcurrentBookingProcess
-  alias TaxiBe.Bookings
 
+  @impl true
   def join("customer:" <> _username, _payload, socket) do
     {:ok, socket}
   end
 
+  @impl true
   def handle_in("request_ride", payload, socket) do
     fake_booking = %{
       id: Ecto.UUID.generate(),
@@ -16,15 +17,21 @@ defmodule TaxiBeWeb.CustomerChannel do
       status: "searching"
     }
 
-    {:ok, _pid} = GenServer.start_link(
-      ConcurrentBookingProcess,
-      %{booking: fake_booking, customer_pid: self()},
+    start_options = [
       name: {:via, Registry, {TaxiBe.ConcurrentBookingRegistry, fake_booking.id}}
-    )
+    ]
 
-    {:reply, {:ok, %{status: "Searching for a driver...", booking_id: fake_booking.id}}, socket}
+    init_args = %{
+      booking: fake_booking,
+      customer_pid: self()
+    }
+
+    {:ok, _pid} = GenServer.start_link(ConcurrentBookingProcess, init_args, start_options)
+
+    {:reply, {:ok, %{booking_id: fake_booking.id}}, socket}
   end
 
+  @impl true
   def handle_in("cancel_ride", %{"booking_id" => booking_id}, socket) do
     case Registry.lookup(TaxiBe.ConcurrentBookingRegistry, booking_id) do
       [{pid, _}] ->
@@ -37,7 +44,6 @@ defmodule TaxiBeWeb.CustomerChannel do
             push(socket, "ride_cancelled_successfully", %{status: "cancelled", charge: 20})
         end
 
-        # Detenemos el proceso de booking después de manejar la cancelación
         GenServer.stop(pid)
       _ ->
         :ok
@@ -45,11 +51,13 @@ defmodule TaxiBeWeb.CustomerChannel do
     {:noreply, socket}
   end
 
+  @impl true
   def handle_info({:ride_accepted, data}, socket) do
     push(socket, "ride_accepted", data)
     {:noreply, socket}
   end
 
+  @impl true
   def handle_info({:ride_not_fulfilled, data}, socket) do
     push(socket, "ride_not_fulfilled", data)
     {:noreply, socket}
